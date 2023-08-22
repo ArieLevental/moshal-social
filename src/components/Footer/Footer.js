b2e2be901f3a6a268e40f571cc3b768e5d81cecd
Arie Levental <arieka39@gmail.com>
2023-08-22 20:16:26 +0300

    initial app skeleton

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,12 @@
+import "./Footer.css";
+
+export const Footer = () =>
+{
+    return (
+        <div className="footer">
+            <div className="footer-item">Terms of Service</div>
+            <div className="footer-item">Privacy Policy</div>
+            <div className="footer-item">© Copyrights</div>
+        </div>
+    )
+}
---
+++ src/components/Main/SideBar/SideBar.js
@@ -0,0 +1,11 @@
+import "./SideBar.css";
+
+export const SideBar = () => {
+  return (
+    <div>
+      <div className="sidebar-item">Homepage</div>
+      <div className="sidebar-item">Users database</div>
+      <div className="sidebar-item">Job sharing</div>
+    </div>
+  );
+};
