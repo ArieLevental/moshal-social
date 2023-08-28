c79cfa08f8c927d0a13056a6f65b59e93deb0f01
Arie Levental <arieka39@gmail.com>
2023-08-28 17:43:25 +0300

    router , scenes, db
    Co-authored-by: Adir Barak <adir-barak@users.noreply.github.com>

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,21 @@
 import { Footer } from "./components/Footer/Footer";
 import { Toolbar } from "./components/Toolbar/Toolbar";
 import { Main } from "./components/Main/Main";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import ProfilePage from "./scenes/profilePage";
 
 function App() {
   return (
     <div className="App">
-      <Toolbar />
-      <Main />
-      <Footer />
+      <BrowserRouter>
+        <Toolbar />
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/users" element={<Main />} />
+          <Route path="/user/:userId" element={<ProfilePage />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </div>
   );
 }
