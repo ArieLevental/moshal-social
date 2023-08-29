57c94005979ba7245610c5bfad98736b2dbe07df
adir-barak <adir.barak@mail.huji.ac.il>
2023-08-29 13:30:32 +0300

    some frontend features like search and random moshalnik

--- client/src/scenes/usersPage/index.js
+++ client/src/scenes/usersPage/index.js
@@ -1,16 +1,26 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 import "./index.css";
 import DataBaseContainer from "./DataBaseContainer/DataBaseContainer";
 import SearchBar from "./SearchBar/SearchBar";
 
 function UsersPage() {
-  const [presentedData, setPresentedData] = useState("");
+  const [usersData, setUsersData] = useState(null);
+  const [presentedData, setPresentedData] = useState(usersData);
+
+  useEffect(() => {
+    fetch(`http://localhost:3001/users`)
+      .then((res) => res.json())
+      .then((jsonData) => {
+        setUsersData(jsonData);
+        setPresentedData(jsonData);
+      });
+  }, []); // string "microsoft"
 
   return (
     <div className="database">
       <h1>Users</h1>
-      <SearchBar setPresentedData={setPresentedData} />
+      <SearchBar usersData={usersData} setPresentedData={setPresentedData} />
       <DataBaseContainer presentedData={presentedData} />
     </div>
   );
