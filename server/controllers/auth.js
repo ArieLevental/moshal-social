5534af01e40499ae263d814cdc026c01e559233a
adir-barak <adir.barak@mail.huji.ac.il>
2023-08-29 17:59:03 +0300

    register is somewhat working

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -9,7 +9,7 @@ export const register = async (req, res) => {
     const salt = await bcrypt.genSalt();
     const passwordHash = await bcrypt.hash(password, salt);
 
-    const newUser = new User({ firstName, lastName, email, passwordHash });
+    const newUser = new User({ firstName, lastName, email, password:passwordHash });
     const savedUser = await newUser.save();
     res.status(201).json(savedUser);
   } catch (err) {
