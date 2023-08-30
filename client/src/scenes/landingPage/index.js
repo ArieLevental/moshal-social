54fc940e01ff9cbb10295f9d5ac5708a58778972
Arie Levental <arieka39@gmail.com>
2023-08-30 16:18:34 +0300

    Added global state and login functionality with jwt token
    Co-authored-by: Adir Barak <adir-barak@users.noreply.github.com>

--- client/src/scenes/landingPage/index.js
+++ client/src/scenes/landingPage/index.js
@@ -7,41 +7,51 @@ import {
   PasswordRecovery,
 } from "./formsContainer/formsContainer";
 
+import { createContext } from "react";
+export const authContext = createContext();
+
+const INITIAL_FORMDATA = {
+  firstName: "",
+  lastName: "",
+  email: "",
+  phoneNumber: "",
+  password: "",
+  passwordConfirmation: "",
+};
+
 const LandingPage = () => {
   const [activeForm, setActiveForm] = useState("login");
-  const [formData, setFormData] = useState({
-    firstName: "",
-    lastName: "",
-    email: "",
-    phoneNumber: "",
-    password: "",
-    passwordConfirmation: "",
-  });
+  const [formData, setFormData] = useState(INITIAL_FORMDATA);
 
   return (
     <div>
-      {/* Render the active form based on the state */}
-      {activeForm === "login" && (
-        <Login
-          setActiveForm={setActiveForm}
-          formData={formData}
-          setFormData={setFormData}
-        />
-      )}
-      {activeForm === "register" && (
-        <Register
-          setActiveForm={setActiveForm}
-          formData={formData}
-          setFormData={setFormData}
-        />
-      )}
-      {activeForm === "recovery" && (
-        <PasswordRecovery
-          setActiveForm={setActiveForm}
-          formData={formData}
-          setFormData={setFormData}
-        />
-      )}
+      <authContext.Provider value={[INITIAL_FORMDATA, setActiveForm, formData, setFormData]}>
+        {/* Render the active form based on the state */}
+        {activeForm === "login" && (
+          <Login
+            // setActiveForm={setActiveForm}
+            // formData={formData}
+            // setFormData={setFormData}
+            // initialFormData={INITIAL_FORMDATA}
+          />
+        )}
+        {activeForm === "register" && (
+          <Register
+            // setActiveForm={setActiveForm}
+            // formData={formData}
+            // setFormData={setFormData}
+            // initialFormData={INITIAL_FORMDATA}
+          />
+        )}
+        {activeForm === "recovery" && (
+          <PasswordRecovery
+            // setActiveForm={setActiveForm}
+            // formData={formData}
+            // setFormData={setFormData}
+            // initialFormData={INITIAL_FORMDATA}
+          />
+        )}
+      </authContext.Provider>
     </div>
   );
 };
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,9 +1,9 @@
 import express from "express";
-import { register } from "../controllers/auth.js";
+import { login, register } from "../controllers/auth.js";
 
 const authRouter = express.Router();
 
-// authRouter.post("/login", login);
+authRouter.post("/login", login);
 
 authRouter.post("/register", register);
 
