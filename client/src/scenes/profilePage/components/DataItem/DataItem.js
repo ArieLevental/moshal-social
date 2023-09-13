537e140a2412a3772dafabfdda1ef02dd23b6f40
Arie Levental <arieka39@gmail.com>
2023-09-13 14:48:22 +0300

    Experience component and styling

---
+++ client/src/scenes/profilePage/components/DataItem/DataItem.js
@@ -0,0 +1,17 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import "./DataItem.css";
+
+const DataItem = ({ faIconName, itemLabel, itemValue }) => {
+  return (
+    <div className="profile-page-data-item">
+      <div className="profile-page-data-item-label">
+        <FontAwesomeIcon icon={faIconName} /> <strong>{itemLabel}:</strong>{" "}
+      </div>
+      <div className="profile-page-data-item-input">
+        {itemValue || "Not provided"}
+      </div>
+    </div>
+  );
+};
+
+export default DataItem;
